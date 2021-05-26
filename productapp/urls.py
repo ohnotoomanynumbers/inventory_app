fa21ee824e27fa9c31346afe04905dafb84d46aa
kanchan-pat <82234347+kanchan-pat@users.noreply.github.com>
2021-05-26 16:03:22 -0400

    Kanchan - 05/26

--- productapp/urls.py
+++ productapp/urls.py
@@ -5,8 +5,8 @@ from productapp import views
 app_name = 'productapp'
 
 urlpatterns=[
-    path('', views.CategoryList.as_view(), name='category_list'),
-    path('<int:category_id>/', views.ProductList, name='product_list'),
-    path('<int:question_id>/<int:product_id>', views.ProductDetail, name='product_detail'),
+    path('', views.CategoryList.as_view(), name='category_list'), # localhost/product/
+    path('<int:category_id>/', views.ProductList, name='product_list'), # localhost/product/1
+    path('<int:question_id>/<int:product_id>', views.ProductDetail, name='product_detail'), # localhost/product/1/1
 
 ]
